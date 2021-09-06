1071cf189b2aa6298c5c27d9f04e9efb614adbff
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-09-06 16:05:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import streamlit.components.v1 as components
 import time
-os.environ['DISPLAY']= ':0'
-import pyautogui
 import base64
 LOGO_IMAGE = "./Logo.png"
 
